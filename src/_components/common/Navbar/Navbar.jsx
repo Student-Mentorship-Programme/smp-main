99edbee5fb8ae4199ff30dc03e2afa2b7e30fee6
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-03-31 19:29:08 +0530

    navbar responsive for mobile

--- src/_components/common/Navbar/Navbar.jsx
+++ src/_components/common/Navbar/Navbar.jsx
@@ -1,56 +1,70 @@
-"use client"
-import smpLogo from "@/images/SMP-Logo.png"
-import styles from "./Navbar.module.css"
-import Image from 'next/image'
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useState, useEffect } from "react"
+"use client";
+import smpLogo from "@/images/SMP-Logo.png";
+import hamburger from "@/images/pages/home/hamburger.svg";
+import styles from "./Navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
-    // console.log(typeof window !== 'undefined');
-    const router = useRouter()
-    const [activeNavElement, setActiveNavElement] = useState(null)
+  // console.log(typeof window !== 'undefined');
+  const router = useRouter();
+  const [activeNavElement, setActiveNavElement] = useState(null);
 
-    const handleNavElementClick = (index) => {
-        setActiveNavElement(index)
-    }
+  const handleNavElementClick = (index) => {
+    setActiveNavElement(index);
+  };
 
-    const handleNavButtonClick = () => {
-        setActiveNavElement(4)
-        router.push('/about-us')
-    }
+  const handleNavButtonClick = () => {
+    setActiveNavElement(4);
+    router.push("/about-us");
+  };
 
-    return (
-        <div className={styles.navbar_main}>
-            <div className={styles.navbar_main_left}>
-                <div className={styles.img_holder_navbar}>
-                    <Image
-                        src={smpLogo}
-                    />
-                </div>
-                <div className={styles.smp_heading_navbar}>
-                    <Link href={"/"} onClick={() => setActiveNavElement(null)}><span style={{ color: '#ed2b2b' }}>SMP</span><span style={{ color: '#073C52' }}>-IITR</span></Link>
-                </div>
-            </div>
-            <div className={styles.navbar_main_right}>
-                <ul>
-                    {["freshers-section : Freshers Section", "mentors : Mentors", "blogs : Blogs", "events : Events"].map((section, index) => (
-                        <li key={index} onClick={() => handleNavElementClick(index)} className={activeNavElement === index ? styles.active_option : ""}>
-                            <Link href={`/${section.split(':')[0].trim()}`}>
-                                {section.split(':')[1].trim()}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <button
-                    onClick={handleNavButtonClick}
-                // className={activeNavElement === 4 ? styles.active_button : ""}
-                >
-                    About us
-                </button>
-            </div>
+  return (
+    <div className={styles.navbar_main}>
+      <div className={styles.navbar_main_left}>
+        <div className={styles.img_holder_navbar}>
+          <Image src={smpLogo} />
         </div>
-    );
-}
+        <div className={styles.smp_heading_navbar}>
+          <Link href={"/"} onClick={() => setActiveNavElement(null)}>
+            <span style={{ color: "#ed2b2b" }}>SMP</span>
+            <span style={{ color: "#073C52" }}>-IITR</span>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.navbar_main_right}>
+        <ul>
+          {[
+            "freshers-section : Freshers Section",
+            "mentors : Mentors",
+            "blogs : Blogs",
+            "events : Events",
+          ].map((section, index) => (
+            <li
+              key={index}
+              onClick={() => handleNavElementClick(index)}
+              className={activeNavElement === index ? styles.active_option : ""}
+            >
+              <Link href={`/${section.split(":")[0].trim()}`}>
+                {section.split(":")[1].trim()}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <button
+          onClick={handleNavButtonClick}
+          // className={activeNavElement === 4 ? styles.active_button : ""}
+        >
+          About us
+        </button>
+        <div className={styles.navbar_main_right_mobile}>
+          <Image src={hamburger}></Image>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
