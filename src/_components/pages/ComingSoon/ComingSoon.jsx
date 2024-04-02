69f6bd5094d5fc7c26ce47ec56518bba6b66d652
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-04-02 22:11:02 +0530

    fix the mobile view header & also coming soon page

--- src/_components/pages/ComingSoon/ComingSoon.jsx
+++ src/_components/pages/ComingSoon/ComingSoon.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from './ComingSoon.module.css'
+import React from "react";
+import styles from "./ComingSoon.module.css";
+import mainImage from "@/images/pages/coming-soon/coming_soon.svg";
+import Image from "next/image";
 
 function Index() {
   return (
-    <div></div>
-  )
+    <div className={styles.heroSection}>
+      <Image src={mainImage} alt="Coming Soon" ></Image>
+      <h1>Coming soon!</h1>
+    </div>
+  );
 }
 
-export default Index
+export default Index;
