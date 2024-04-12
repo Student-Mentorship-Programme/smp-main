5238c17a9c264cdcede6c37c447dc4b28b3141a0
Mohit Sharma <mohit.vsht@gmail.com>
2024-04-12 22:07:03 +0530

    coming soon update

--- src/_components/common/ComingSoon/ComingSoon.jsx
+++ src/_components/common/ComingSoon/ComingSoon.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./ComingSoon.module.css";
-import mainImage from "@/images/pages/coming-soon/coming_soon.svg";
+import mainImage from "@/images/pages/coming-soon/coming_soon.png";
 import Image from "next/image";
 
 function ComingSoon() {
   return (
     <div className={styles.heroSection}>
       <Image src={mainImage} alt="Coming Soon" ></Image>
-      <h1>Coming soon!</h1>
     </div>
   );
 }
